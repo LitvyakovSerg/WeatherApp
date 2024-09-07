79b24850b813f5e9f1525c9b77b20ef2f7250aba
LitvyakovSerg <freezer.svd@gmail.com>
2024-09-07 16:59:20 +0300

    Add picture of weather and date on forecast fragment. Add optionally picture on weather fragment.

--- app/src/main/java/com/example/android/weatherapp/ForecastAdapter.kt
+++ app/src/main/java/com/example/android/weatherapp/ForecastAdapter.kt
@@ -3,20 +3,83 @@ package com.example.android.weatherapp
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.weatherapp.databinding.ActivityMainBinding
 import com.example.android.weatherapp.databinding.FragmentForecastBinding
 import com.example.android.weatherapp.databinding.FragmentWeatherBinding
 import com.example.android.weatherapp.databinding.ItemForecastBinding
 import com.example.android.weatherapp.model.forecast.ForecastResult
+import com.example.android.weatherapp.repository.WeatherRepositoryImpl
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 
 class ForecastAdapter(private val fragmentContext: Context, private val forecastList: List<ForecastResult>) : RecyclerView.Adapter<ForecastAdapter.ViewHolder>() {
 
    class ViewHolder(private val binding: ItemForecastBinding, private val context: Context) : RecyclerView.ViewHolder(binding.root) {
         fun bindItem(forecast: ForecastResult) {
+
+            val inputDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val outputDateFormat = SimpleDateFormat("d MMMM HH:mm", Locale.getDefault())
+
+            val date = inputDateFormat.parse(forecast.date)
+            val outputDate = date?.let { outputDateFormat.format(it) }
+
+            binding.itemRecyclerDate.text = "Date: ${outputDate}"
             binding.itemRecyclerTemp.text = "Temp: ${forecast.temp}"
             binding.itemRecyclerDescription.text = "Weather: ${forecast.description}"
+
+            when (forecast.main) {
+                WeatherRepositoryImpl.WEATHER_TYPE_CLEAR -> {
+                    binding.weatherImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.sun
+                        )
+                    )
+                }
+                WeatherRepositoryImpl.WEATHER_TYPE_CLOUDS -> {
+                    binding.weatherImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.cloudy
+                        )
+                    )
+                }
+                WeatherRepositoryImpl.WEATHER_TYPE_RAIN -> {
+                    binding.weatherImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.heavy_rain
+                        )
+                    )
+                }
+                WeatherRepositoryImpl.WEATHER_TYPE_SNOW -> {
+                    binding.weatherImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.snow
+                        )
+                    )
+                }
+                WeatherRepositoryImpl.WEATHER_TYPE_THUNDERSTORM -> {
+                    binding.weatherImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.thunderstorm
+                        )
+                    )
+                }
+                else -> {
+                    binding.weatherImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.mist
+                        )
+                    )
+                }
+            }
         }
    }
 
--- app/src/main/java/com/example/android/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/android/weatherapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.android.weatherapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -15,6 +17,7 @@ import com.example.android.weatherapp.network.WeatherApi
 import com.example.android.weatherapp.viewmodel.MainViewModel
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.textfield.TextInputLayout
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -30,6 +33,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var inputField: TextInputLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -37,10 +42,22 @@ class MainActivity : AppCompatActivity() {
 
         viewPager = binding.viewPager
         tabLayout = binding.tabLayout
+        inputField = binding.mainInputField
 
-        lifecycleScope.launch(Dispatchers.Main) {
-            mainViewModel.getCoordinates("London")
+        inputField.setEndIconOnClickListener {
+
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            if (inputMethodManager.isActive) {
+                inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken, 0)
+            }
+
+            lifecycleScope.launch(Dispatchers.Main) {
+                mainViewModel.getCoordinates(inputField.editText?.text.toString())
+            }
         }
+
+
 
         mainViewModel.coordinatesResult.observe(this, {
             lifecycleScope.launch(Dispatchers.Main) {
